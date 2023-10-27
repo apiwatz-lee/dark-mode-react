8e470bc1c631d45c5cba5f463f545b7b3210ae66
Apiwat Lee <apiwatlee@Apiwats-MacBook-Air.local>
2023-10-27 18:01:06 +0700

    darkmode:user can switch light/dark mode

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,17 +1,39 @@
 import React from "react";
 import light from "../image/light.svg";
 import dark from "../image/dark.svg";
+import { useContext } from "react";
+import { ThemeContext } from "../App";
 
 const Content = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <main className="flex items-center h-[80%]">
+    <main
+      className={`flex items-center h-[80%] ${
+        theme === "dark" ? `bg-slate-800` : null
+      }`}
+    >
       <div>
-        <h1 className="text-7xl text-center font-bold">Apiwat Leelasawatsuk</h1>
-        <p className="text-center text-3xl font-bold mt-10">
-          DarkMode Workshop
+        <h1
+          className={`text-7xl text-center font-bold ${
+            theme === `dark` ? `text-slate-200` : null
+          }`}
+        >
+          Apiwat Leelasawatsuk
+        </h1>
+        <p
+          className={`text-center text-3xl font-bold mt-10 ${
+            theme === "dark" ? `text-amber-500` : null
+          }`}
+        >
+          LightMode Workshop
         </p>
       </div>
-      <img src={light} alt="Logo" className="w-[50%]" />
+      <img
+        src={theme === "dark" ? dark : light}
+        alt="Logo"
+        className="w-[50%]"
+      />
     </main>
   );
 };
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import Switch from "react-switch";
+import { useContext } from "react";
+import { ThemeContext } from "../App";
 
 const Title = () => {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  const toggleSwitch = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
   return (
-    <header className="h-[20%] border flex flex-row items-center">
-      <span className="border text-4xl font-bold ml-[100px]">Mode</span>
+    <header
+      className={`h-[20%] flex flex-row items-center justify-around ${
+        theme === "dark" ? `bg-slate-800 text-amber-500` : null
+      }`}
+    >
+      <span className="text-4xl font-bold">Mode {theme}</span>
+      <Switch
+        onChange={toggleSwitch}
+        checked={theme === "dark"}
+        uncheckedIcon={false}
+        checkedIcon={false}
+        onColor={`#ffa500`}
+      />
     </header>
   );
 };
